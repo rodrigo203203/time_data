787175ddae02aa602a115ccda4a6e81cd0f50fdc
Rodrigo Morales Rivas <rodrigo-morales2011@hotmail.es>
2022-10-03 18:58:55 -0400

    Add files via upload

--- test/test_helper.py
+++ test/test_helper.py
@@ -1,93 +1,82 @@
+from datetime import date
 from src.assignment7_1 import get_dates_in_interval
 from src.assignnment7_2 import get_default_date_data
 import unittest
 
 
-class TestDates(unittest.TestCase):
-    # Assignments 7.1
-    def test_one_date(self):
-        start_date = '09/12/2022'
-        end_date = '09/12/2022'
+def test_one_date():
+    start_date = '09/12/2022'
+    end_date = '09/12/2022'
 
-        dates = get_dates_in_interval(start_date, end_date)
-        self.assertEqual('09/12/2022', dates)
+    dates = get_dates_in_interval(start_date, end_date)
+    assert '09/12/2022' in dates
 
-    def test_between_two_dates(self):
-        start_date = '9/12/2022'
-        end_date = '9/15/2022'
+def test_in_two_dates():
+    start_date = '9/12/2022'
+    end_date = '9/15/2022'
 
-        dates = get_dates_in_interval(start_date, end_date)
-        self.assertEqual(['09/12/2022', '09/13/2022', '09/14/2022', '09/15/2022'], dates)
+    dates = get_dates_in_interval(start_date, end_date)
+    assert '09/12/2022' in dates
+    assert '09/13/2022' in dates
+    assert '09/14/2022' in dates
+    assert '09/15/2022' in dates
 
-    def test_between_two_dates_start_bigger_than_end(self):
-        start_date = '9/15/2022'
-        end_date = '9/10/2022'
+def test_start_bigger_than_end():
+    start_date = '9/15/2022'
+    end_date = '9/10/2022'
 
-        dates = get_dates_in_interval(start_date, end_date)
-        self.assertEqual(None, dates)
+    dates = get_dates_in_interval(start_date, end_date)
+    assert dates is None
 
-    def test_between_two_dates_none(self):
-        start_date = None
-        end_date = None
+def test_two_dates_is_none():
+    start_date = None
+    end_date = None
 
-        dates = get_dates_in_interval(start_date, end_date)
-        self.assertEqual(None, dates)
+    dates = get_dates_in_interval(start_date, end_date)
+    assert dates is None 
 
     # Assignments 7.2
-    def test_start_date_and_end_date_equals_default_value_negative_number(self):
-        start_date = '09/12/2022'
-        end_date = '09/12/2022'
-        default_value = -99999
+def test_equals_are_value_negative_number():
+    start_date = '09/12/2022'
+    end_date = '09/12/2022'
+    default_value = -99999
 
-        dates = get_default_date_data(start_date, end_date, default_value)
-        self.assertEqual({'date': '09/12/2022', 'participants': -99999}, dates)
+    dates = get_default_date_data(start_date, end_date, default_value)
+    assert {'date':'09/12/2022', 'participants':-99999} in dates
 
-    def test_start_date_small_than_end_date_default_value_none(self):
-        start_date = '09/12/2022'
-        end_date = '09/15/2022'
-        default_value = None
+def test_date_default_value_none():
+    start_date = '09/12/2022'
+    end_date = '09/15/2022'
+    default_value = None
 
-        dates = get_default_date_data(start_date, end_date, default_value)
-        self.assertEqual(
-            [{'date': '09/12/2022', 'participants': None},
-             {'date': '09/13/2022', 'participants': None},
-             {'date': '09/14/2022', 'participants': None},
-             {'date': '09/15/2022', 'participants': None}],
-            dates
-        )
+    dates = get_default_date_data(start_date, end_date, default_value)
+    assert {'date': '09/12/2022', 'participants': None} in dates
+    assert {'date': '09/13/2022', 'participants': None} in dates
+    assert {'date': '09/14/2022', 'participants': None} in dates
+    assert {'date': '09/15/2022', 'participants': None} in dates
 
-    def test_start_date_bigger_than_end_date_default_value_negative_nine(self):
-        start_date = '09/15/2022'
-        end_date = '09/12/2022'
-        default_value = -99999
 
-        dates = get_default_date_data(start_date, end_date, default_value)
-        self.assertEqual(0, len(dates))
+def test_none_defaut_value_zerohours():
+    start_date = None
+    end_date = None
+    default_value = '0h 0m'
 
-    def test_start_date_and_end_date_none_defaut_value_zerohours(self):
-        start_date = None
-        end_date = None
-        default_value = '0h 0m'
+    dates = get_default_date_data(start_date, end_date, default_value)
+    assert {'date': '09/12/2022', 'participants': '0h 0m'} in dates
+    assert {'date': '09/13/2022', 'participants': '0h 0m'} in dates
+    assert {'date': '09/14/2022', 'participants': '0h 0m'} in dates
+    assert {'date': '09/15/2022', 'participants': '0h 0m'} in dates
 
-        dates = get_default_date_data(start_date, end_date, default_value)
-        self.assertEqual(
-            [{'date': '09/12/2022', 'participants': '0h 0m'},
-             {'date': '09/13/2022', 'participants': '0h 0m'},
-             {'date': '09/14/2022', 'participants': '0h 0m'},
-             {'date': '09/15/2022', 'participants': '0h 0m'}],
-            dates
-        )
 
-    def test_start_date_small_than_end_date_default_value_negative_nine(self):
-        start_date = '09/12/2022'
-        end_date = '09/15/2022'
-        default_value = -99999
+def test_default_value_negative_nine():
+    start_date = '09/12/2022'
+    end_date = '09/15/2022'
+    default_value = -99999
 
-        dates = get_default_date_data(start_date, end_date, default_value)
-        self.assertEqual(
-            [{'date': '09/12/2022', 'participants': -99999},
-             {'date': '09/13/2022', 'participants': -99999},
-             {'date': '09/14/2022', 'participants': -99999},
-             {'date': '09/15/2022', 'participants': -99999}],
-            dates
-        )
+    dates = get_default_date_data(start_date, end_date, default_value)
+    assert {'date': '09/12/2022', 'participants': -99999} in dates
+    assert {'date': '09/13/2022', 'participants': -99999} in dates
+    assert {'date': '09/14/2022', 'participants': -99999} in dates
+    assert {'date': '09/15/2022', 'participants': -99999} in dates
+        
+        
